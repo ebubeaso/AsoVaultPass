d38601722b99fa7edd09b39265f7c0e410765d02
Ebube Aso <aso.ebube1@outlook.com>
2021-06-25 00:04:19 -0400

    still tweaking the password manager frontend

--- ReactFrontend/src/Vault.tsx
+++ ReactFrontend/src/Vault.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { Title, Subtitle } from './Styles';
 
 export const VaultHome: React.FC = () => {
+    var [login, setLogin] = React.useState(false);
+    var [buttonColor, setButtonColor] = React.useState<string>("green");
+    let buttonCss: React.CSSProperties = {backgroundColor: buttonColor}
     return (
         <div>
         <div className="SearchDiv">
             <input type="search" name="search" id="search" placeholder="Search"/>
+            <button style={buttonCss} className="SearchButton" 
+                onMouseOver={() => setButtonColor("#2ddc2d")}
+                onMouseOut={() => setButtonColor("green")}>Search</button>
         </div>
         <Title>My Sites</Title>
         <div className="SiteGrid">
-
+            <div className="GridItem">Facebook</div>
+            <div className="GridItem">Gmail</div>
+            <div className="GridItem">Outlook</div>
+            <div className="GridItem">Dropbox</div>
+            <div className="GridItem">Fashion Nova</div>
+            <div className="GridItem">Bank of America</div>
+            <div className="GridItem">Paypal</div>
         </div>
         </div>
     )
--- ReactFrontend/src/main.js
+++ ReactFrontend/src/main.js
@@ -35493,11 +35493,22 @@ exports.VaultHome = void 0;
 const react_1 = __importDefault(require("react"));
 const Styles_1 = require("./Styles");
 const VaultHome = () => {
+    var [login, setLogin] = react_1.default.useState(false);
+    var [buttonColor, setButtonColor] = react_1.default.useState("green");
+    let buttonCss = { backgroundColor: buttonColor };
     return (react_1.default.createElement("div", null,
         react_1.default.createElement("div", { className: "SearchDiv" },
-            react_1.default.createElement("input", { type: "search", name: "search", id: "search", placeholder: "Search" })),
+            react_1.default.createElement("input", { type: "search", name: "search", id: "search", placeholder: "Search" }),
+            react_1.default.createElement("button", { style: buttonCss, className: "SearchButton", onMouseOver: () => setButtonColor("#2ddc2d"), onMouseOut: () => setButtonColor("green") }, "Search")),
         react_1.default.createElement(Styles_1.Title, null, "My Sites"),
-        react_1.default.createElement("div", { className: "SiteGrid" })));
+        react_1.default.createElement("div", { className: "SiteGrid" },
+            react_1.default.createElement("div", { className: "GridItem" }, "Facebook"),
+            react_1.default.createElement("div", { className: "GridItem" }, "Gmail"),
+            react_1.default.createElement("div", { className: "GridItem" }, "Outlook"),
+            react_1.default.createElement("div", { className: "GridItem" }, "Dropbox"),
+            react_1.default.createElement("div", { className: "GridItem" }, "Fashion Nova"),
+            react_1.default.createElement("div", { className: "GridItem" }, "Bank of America"),
+            react_1.default.createElement("div", { className: "GridItem" }, "Paypal"))));
 };
 exports.VaultHome = VaultHome;
 
